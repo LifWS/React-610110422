79b020f72a20a437fe0df40e2e9d6a699a4b3e2b
LifWS <6010110422@psu.ac.th>
2019-09-20 02:29:20 +0700

    test commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div><WordCard value="12345678910" /></div>
+      <div><WordCard value="1Test Commit" /></div>
       
     );
   }
